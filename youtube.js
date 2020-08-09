140b38c41527271d991e17cf946d0615b9502f14
DarrenYang <telunyang@gmail.com>
2020-08-09 22:42:09 +0800

    更新 youtube 元素相關選擇器 (css selector)

--- youtube.js
+++ youtube.js
@@ -1,11 +1,10 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true, width: 1280, height: 1024 });
+const nightmare = Nightmare({ show: true, width: 1024, height: 960 });
 const fs = require('fs');
 
 //引入 jQuery 機制
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
+const { JSDOM } = require('jsdom');
+const { window } = new JSDOM("");
 const $ = require('jquery')(window);
 
 //設定 request headers
@@ -23,8 +22,8 @@ let strSinger = '張學友';
 
 //初始化設定
 async function init(){
-    if( !fs.existsSync(`downloads`) ){
-        fs.mkdirSync(`downloads`);
+    if( !fs.existsSync(`downloads/youtube`) ){
+        fs.mkdirSync(`downloads/youtube`);
     }
 }
 
@@ -36,30 +35,58 @@ async function search(){
     .goto('https://www.youtube.com', headers)
     .type('input[id="search"]', strSinger)
     .click('button#search-icon-legacy')
-    .catch((err) => {
-        console.error(err);
-    });
+    .wait(2000);
+}
+
+//操作篩選器
+async function filter(){
+    console.log(`操作篩選器`);
+
+    await nightmare
+    .click('a.yt-simple-endpoint.style-scope.ytd-toggle-button-renderer') //按下篩選器連結來開啟選項
+    .wait(6000)
+    .click('ytd-search-filter-group-renderer:nth-of-type(5) ytd-search-filter-renderer:nth-of-type(3) a#endpoint') //按下「觀看次數」
+    .wait(2000)
+    .click('a.yt-simple-endpoint.style-scope.ytd-toggle-button-renderer') //按下篩選器連結來開啟選項
+    .wait(6000)
+    .click('ytd-search-filter-group-renderer:nth-of-type(3) ytd-search-filter-renderer:nth-of-type(1) a#endpoint') //按下「短片 (不到 4 分鐘)」
+    .wait(2000)
+    .click('a.yt-simple-endpoint.style-scope.ytd-toggle-button-renderer') //按下篩選器連結來開啟選項
+    .wait(6000);
 }
 
 //滾動頁面，將動態資料逐一顯示出來
 async function scroll(){
     console.log(`準備滾動頁面`);
 
-    let currentHeight = 0; //window 裡面內容的當前總高度
-    let offset = 0; //總偏移量
+    /**
+     * innertHeightOfWindow: 視窗內 document 區域的內部高度
+     * totalOffset: 目前滾動的距離
+     */
+    let innerHeightOfWindow = 0, totalOffset = 0;
 
     //不斷地滾動，直到沒有辦法再往下滾
-    while(offset <= currentHeight){ // 2000 <= 50000
-        currentHeight = await nightmare.evaluate(()=>{
-            return document.documentElement.scrollHeight; //回傳瀏覽器當前已滾動的高度
+    while(totalOffset <= innerHeightOfWindow){ // 2000 <= 50000
+        //取得視窗內 document 區域的內部高度
+        innerHeightOfWindow = await nightmare.evaluate(() => {
+            return document.documentElement.scrollHeight;
         });
+        //增加滾動距離的數值
+        totalOffset += 500;
 
-        //每次滾動 500 單位的距離，offset 需要累加，才能對應到合適的距離
-        offset += 500;
-        await nightmare.scrollTo(offset, 0).wait(500);
+        //滾動到 totalOffset 指定的距離
+        await nightmare.scrollTo(totalOffset, 0).wait(500);
 
-        if(offset > 1000){
-            break; //滾動一段高度後，強迫跳出迴圈；視情況使用
+        console.log(`totalOffset = ${totalOffset}, innerHeightOfWindow = ${innerHeightOfWindow}`);
+
+        //接近底部時，按下一頁
+        if( (innerHeightOfWindow - totalOffset) < 2000 && await nightmare.exists('button.b-btn.b-btn--link.js-more-page')){
+            await _checkPagination();
+        }
+
+        //測試用，滾動的距離超過 500，則 scroll() 執行完畢，往下一個 function 繼續執行
+        if( totalOffset > 500 ){
+            break;
         }
     }
 }
@@ -68,11 +95,13 @@ async function scroll(){
 async function parse(){
     console.log(`分析、整理、收集重要資訊...`);
 
-    let html = await nightmare.evaluate(() => {
+    let html = await nightmare
+    .wait('div#contents.style-scope.ytd-item-section-renderer ytd-video-renderer.style-scope.ytd-item-section-renderer')
+    .evaluate(() => {
         return document.documentElement.innerHTML;
     });
 
-    let pattern = null;
+    let regex = null;
     let arrMatch = null;
     let obj = {};
 
@@ -82,8 +111,8 @@ async function parse(){
         //找出縮圖連結 & 影片 ID
         let linkOfImage = $(element).find('img#img.style-scope.yt-img-shadow').attr('src');
 
-        pattern = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_]{11})\/hqdefault\.jpg/g;
-        if( (arrMatch = pattern.exec(linkOfImage)) != null ){
+        regex = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_]{11})\/hqdefault\.jpg/g;
+        if( (arrMatch = regex.exec(linkOfImage)) != null ){
             obj.img = arrMatch[0]; //縮圖連結
             obj.id = arrMatch[1]; //從連結擷取出來的 video id (watch?v=xxxxxxxxxxx)
 
@@ -130,26 +159,37 @@ async function visit(){
         .text();
 
         //取得觀看次數
-        let pattern = /[0-9,]+/g;
-        let match = null;
-        match = pattern.exec(strPageView);
+        let regex = /[0-9,]+/g;
+        let match = regex.exec(strPageView);
         strPageView = match[0];
         strPageView = strPageView.replace(/,/g, ''); //剩下 '545056'
 
         //取得按讚次數
-        let strLikeCount = $(html).find('div#top-level-buttons yt-formatted-string#text:eq(0)').attr('aria-label');
-        pattern = /[0-9,]+/g;
-        match = pattern.exec(strLikeCount);
-        strLikeCount = match[0];
-        strLikeCount = strLikeCount.replace(/,/g, '');
-
+        let strLikeCount = $(html)
+        .find('div#top-level-buttons ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button.style-text:nth-of-type(1) yt-formatted-string#text')
+        .attr('aria-label');
+        regex = /[0-9,]+/g;
+        match = regex.exec(strLikeCount);
+        if( match !== null){
+            strLikeCount = match[0];
+            strLikeCount = strLikeCount.replace(/,/g, '');
+        } else {
+            strLikeCount = 0;
+        }
+        
         //取得不喜歡次數
-        let strUnlikeCount = $(html).find('div#top-level-buttons yt-formatted-string#text:eq(1)').attr('aria-label');
-        pattern = /[0-9,]+/g;
-        match = pattern.exec(strUnlikeCount);
-        strUnlikeCount = match[0];
-        strUnlikeCount = strUnlikeCount.replace(/,/g, '');
-
+        let strUnlikeCount = $(html)
+        .find('div#top-level-buttons ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button.style-text:nth-of-type(2) yt-formatted-string#text')
+        .attr('aria-label');
+        regex = /[0-9,]+/g;
+        match = regex.exec(strUnlikeCount);
+        if( match !== null){
+            strUnlikeCount = match[0];
+            strUnlikeCount = strUnlikeCount.replace(/,/g, '');
+        } else {
+            strUnlikeCount = 0;
+        }
+        
         //建立新屬性
         arrLink[i].pageView = parseInt(strPageView);
         arrLink[i].likeCount = parseInt(strLikeCount);
@@ -174,7 +214,15 @@ async function asyncArray(functionList){
 
 (
     async function (){
-        await asyncArray([init, search, scroll, parse, visit, close]).then(async ()=>{
+        await asyncArray([
+            init, 
+            search, 
+            filter, 
+            scroll, 
+            parse, 
+            visit, 
+            close
+        ]).then(async ()=>{
             console.dir(arrLink, {depth: null});
     
             //新增檔案，同時寫入內容
