6b25330def5961fb4e95d9d863f77475ee38b89a
DarrenYang <telunyang@gmail.com>
2020-08-09 22:42:42 +0800

    新增下載提示(console.log)

--- youtube_downloader.js
+++ youtube_downloader.js
@@ -6,13 +6,16 @@ const exec = util.promisify( require('child_process').exec );
     async function () {
         let strJson = await fs.readFileSync('downloads/youtube.json', { encoding: 'utf-8' });
         let arrJson = JSON.parse(strJson);
-        // console.dir(arrJson, {depth: null});
+
+        //若沒資料夾，則直接建立
+        //(註: existsSync、mkdirSync 與 exists、mkdir 的差異，在於有 sync 的函式，不需要 callback)
+        if (! await fs.existsSync(`downloads/youtube`) ){ 
+            await fs.mkdirSync(`downloads/youtube`, {recursive: true}); //遞迴建立資料夾
+        }
 
         for(let i = 0; i < arrJson.length; i++){
-            await exec(`youtube-dl.exe -f mp4 -i ${arrJson[i].link} -o "downloads/%(id)s.%(ext)s"`);
-            if(i === 2){
-                break;
-            }
+            console.log(`下載中 ==> 連結${arrJson[i].link}, 名稱: ${arrJson[i].title}`);
+            await exec(`youtube-dl.exe -f mp4 -i ${arrJson[i].link} -o "downloads/youtube/%(id)s.%(ext)s"`);
         }
     }
 )();
